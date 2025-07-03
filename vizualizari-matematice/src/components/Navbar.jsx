8c937618edddeae9e8fdca2c1a2eae97fdb2cc9a
Cerescu Marius <marius.cerescu@gmail.com>
2025-07-03 10:09:23 +0300

    Refactor Navbar to use NavLink for active link styling and update CSS for active link appearance

--- vizualizari-matematice/src/components/Navbar.jsx
+++ vizualizari-matematice/src/components/Navbar.jsx
@@ -1,31 +1,32 @@
 // src/components/Navbar.jsx
 
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 import { useLanguage } from '../contexts/LanguageContext';
 import LanguageSwitcher from './LanguageSwitcher';
 
 function Navbar() {
   const { t } = useLanguage();
+  const getLinkClass = ({ isActive }) => isActive ? 'active-link' : '';
 
   return (
     <nav className="navbar">
-      <Link to="/" className="nav-brand">{t('home')}</Link>
+      <NavLink to="/" className="nav-brand">{t('home')}</NavLink>
       <ul className="nav-links">
-        <li><Link to="/t47">{t('p_t47')}</Link></li>
-        <li><Link to="/t59">{t('p_t59')}</Link></li>
-        <li><Link to="/t55">{t('p_t55')}</Link></li>
-        <li><Link to="/td04">{t('p_td04')}</Link></li>
-        <li><Link to="/tfig01">{t('p_tfig01')}</Link></li>
-        <li><Link to="/tfig06">{t('p_tfig06')}</Link></li>
-        <li><Link to="/tfig07">{t('p_tfig07')}</Link></li>
-        <li><Link to="/tfig08">{t('p_tfig08')}</Link></li>
-        <li><Link to="/tfig10">{t('p_tfig10')}</Link></li>
-        <li><Link to="/td20">{t('p_td20')}</Link></li>
-        <li><Link to="/t46">{t('p_t46')}</Link></li>
-        <li><Link to="/t48">{t('p_t48')}</Link></li>
-        <li><Link to="/t53">{t('p_t53')}</Link></li>
+        <li><NavLink to="/t47" className={getLinkClass}>{t('p_t47')}</NavLink></li>
+        <li><NavLink to="/t59" className={getLinkClass}>{t('p_t59')}</NavLink></li>
+        <li><NavLink to="/t55" className={getLinkClass}>{t('p_t55')}</NavLink></li>
+        <li><NavLink to="/td04" className={getLinkClass}>{t('p_td04')}</NavLink></li>
+        <li><NavLink to="/tfig01" className={getLinkClass}>{t('p_tfig01')}</NavLink></li>
+        <li><NavLink to="/tfig06" className={getLinkClass}>{t('p_tfig06')}</NavLink></li>
+        <li><NavLink to="/tfig07" className={getLinkClass}>{t('p_tfig07')}</NavLink></li>
+        <li><NavLink to="/tfig08" className={getLinkClass}>{t('p_tfig08')}</NavLink></li>
+        <li><NavLink to="/tfig10" className={getLinkClass}>{t('p_tfig10')}</NavLink></li>
+        <li><NavLink to="/td20" className={getLinkClass}>{t('p_td20')}</NavLink></li>
+        <li><NavLink to="/t46" className={getLinkClass}>{t('p_t46')}</NavLink></li>
+        <li><NavLink to="/t48" className={getLinkClass}>{t('p_t48')}</NavLink></li>
+        <li><NavLink to="/t53" className={getLinkClass}>{t('p_t53')}</NavLink></li>
       </ul>
       <LanguageSwitcher />
     </nav>
